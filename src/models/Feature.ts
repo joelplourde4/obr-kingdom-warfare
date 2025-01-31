41a255b622267b2188b3dd847bec2d3310d898e1
joelplourde4 <jplourde@cloudops.com>
2025-01-31 16:38:10 -0500

    Fix an issue where the tooltip prevents adding things

--- src/models/Feature.ts
+++ src/models/Feature.ts
@@ -1,10 +1,12 @@
 export class Feature {
+    id: number;
     name: string;
     source: string;
     description: string;
     visible: boolean;
 
-    constructor() {
+    constructor(id: number) {
+        this.id = id;
         this.name = "Default"
         this.source = ""
         this.description = ""
